69fd919861f085c89b215fc7fff2e19924717171
Diamond <diamondobama@gmail.com>
2025-08-25 22:27:56 +0200

    Uses the Geo traits directly in the Geo class

--- src/Facade/GeoFacade.php
+++ src/Facade/GeoFacade.php
@@ -1,24 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DiamondDev\GeographicalCalculator\Facade;
 
 use DiamondDev\GeographicalCalculator\Classes\Geo;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method Geo setPoint(array $point)
- * @method Geo setPoints(array $points)
- * @method Geo setOptions(array $options)
- * @method Geo setMainPoint(array $point)
- * @method Geo setDiameter(int $diameter)
- * @method Geo clearResult()
- * @method Geo getDistance($callback)
- * @method Geo getCenter($callback)
- * @method Geo allFeatures($callback)
- * @method Geo getClosest()
- * @method Geo getFarthest()
- * @method Geo getOrderByNearestNeighbor()
- * @method Geo isInArea()
+ * @method static Geo setPoint(array $point)
+ * @method static Geo setPoints(array $points)
+ * @method static Geo setOptions(array $options)
+ * @method static Geo setMainPoint(array $point)
+ * @method static Geo setDiameter(int $diameter)
+ * @method static Geo clearResult()
+ * @method static Geo getDistance($callback)
+ * @method static Geo getCenter($callback)
+ * @method static Geo allFeatures($callback)
+ * @method static Geo getClosest()
+ * @method static Geo getFarthest()
+ * @method static Geo getOrderByNearestNeighbor()
+ * @method static Geo isInArea()
  */
 class GeoFacade extends Facade
 {
